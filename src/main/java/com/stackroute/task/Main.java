06f0d00c195c2488a9a9f714a4db31ef045e9b85
Ashhwini <ashwinih378@gmail.com>
2019-03-04 22:56:29 +0530

    task5

--- src/main/java/com/stackroute/task/Main.java
+++ src/main/java/com/stackroute/task/Main.java
@@ -3,12 +3,14 @@ package com.stackroute.task;
 import com.stackroute.domain.Movie;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+        AbstractApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
+        applicationContext.registerShutdownHook();
         Movie movie = (Movie)applicationContext.getBean("movie");
         System.out.println(movie.getActor());
 
